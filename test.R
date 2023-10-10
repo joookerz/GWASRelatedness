1694a0b56c6e030fe534638697dbbdbf4ca24545
Your Name <you@example.com>
2023-10-10 12:06:09 +0800

    test2

--- test.R
+++ test.R
@@ -1,3 +1,12 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+install.packages("usethis")
+library(usethis)
+git_sitrep()
+
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+
+use_git_config(user.name = "joookerz", user.email = "1435138480@qq.com")
