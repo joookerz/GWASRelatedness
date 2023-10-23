039b312bb4c42c64a1d1fa4ee61de8dfeefdcb6e
joookerz <joookerz@MacBook-Pro.local>
2023-10-23 11:50:51 +0800

    SNPSelection

--- relatedness200.R
+++ relatedness200.R
@@ -1,8 +1,8 @@
 library(SNPRelate)
 library(ggplot2)
 
-plink_path <- "C:/Users/86180/Desktop/genetic/euro_10Ksnp"
-out_gds_file <- "C:/Users/86180/Desktop/genetic/euro_10Ksnp.gds"
+plink_path <- "~/Desktop/project_management/GWASrelatedness/data/euro_10Ksnp"
+out_gds_file <- "~/Desktop/project_management/GWASrelatedness/data/euro_10Ksnp.gds"
 
 # 读取 PLINK 文件并生成 GDS 文件
 snpgdsBED2GDS(bed.fn = paste0(plink_path, ".bed"), bim.fn = paste0(plink_path, ".bim"), fam.fn = paste0(plink_path, ".fam"), out.gdsfn = out_gds_file)
@@ -15,8 +15,8 @@ dim(genotype_matrix)
 summary_data$sample.id
 #genotype_matrix <- genotype_matrix[1:20,]
                 
-subset_matrix <- genotype_matrix[1:200, 1:10]
-print(subset_matrix)
+#subset_matrix <- genotype_matrix[1:200, 1:10]
+#print(subset_matrix)
 
 #填充后矩阵
 genotype_matrix1 <- genotype_matrix
@@ -68,8 +68,8 @@ for(i in 1:m){
   
 }
 
-subset_matrix1 <- genotype_matrix1[1:200, 1:10]
-print(subset_matrix1)
+#subset_matrix1 <- genotype_matrix1[1:200, 1:10]
+#print(subset_matrix1)
 
 x <- genotype_matrix1[1:200,]
 n <- nrow(x)
@@ -144,8 +144,8 @@ hist(fdr)
 ######################################################################################################################
 
 
-write.csv(theta, "C:/Users/86180/Desktop/genetic/theta.csv")
-write.csv(P, "C:/Users/86180/Desktop/genetic/P.csv")
+#write.csv(theta, "C:/Users/86180/Desktop/genetic/theta.csv")
+#write.csv(P, "C:/Users/86180/Desktop/genetic/P.csv")
  
 eg <- eigen(G)
 barplot(eg$values)
@@ -209,6 +209,63 @@ ggplot(data = data)+geom_segment(aes(x =s1, y =0,xend =s1, yend =s2, color= as.f
   theme(legend.position = 'none')+ 
   annotate(geom = 'segment',x=0,xend=nrow(data),y=quantile(data$s2,0.95), 
            yend=quantile(data$s2,0.95),lty=4,color='black')
+
+###################################################################################################################
+#SNPselection
+
+effect <- data.frame(s1=NA,s2=NA,s3=NA,s4=NA)
+for(i in 1:40){
+  P_control <- i*5e-3
+  del <- as.integer(m*P_control)
+  data_ranked <- data[order(data$s2, decreasing = TRUE),]
+  del_number <- data_ranked$s1[1:del]
+  #data_selected <- data_ranked[-(1:del),]
+  #plot(data_selected$s2)
+  #plot(data_ranked$s2)
+  
+  genotype_matrix2_hat <- genotype_matrix2[,-del_number]
+  dim(genotype_matrix2_hat)
+  m_hat <- ncol(genotype_matrix2_hat)
+  n <- nrow(genotype_matrix2_hat)
+  
+  A = genotype_matrix2_hat^2
+  V <- apply(A, 1, sum)/m_hat
+  G <- (genotype_matrix2_hat %*% t(genotype_matrix2_hat)) /m_hat
+  G0 <- G[col(G)<row(G)]
+  me <- 1/var(G0)
+  effect[i,]$s1 <- P_control
+  effect[i,]$s2 <- m
+  effect[i,]$s3 <- me
+  effect[i,]$s4 <- m/me
+}
+plot(effect$s1,effect$s4,xlab="P_control",ylab="m/me")
+
+#####################
+theta <- matrix(data=NA, nrow=n, ncol=n)
+for(i in 1:n){
+  for(j in 1:n){
+    theta[i,j] <- 1-0.5*(V[i]+V[j]-2*G[i,j])
+  }
+}
+
+Z <- matrix(data=NA, nrow=n, ncol=n)
+for(i in 1:n){
+  for(j in 1:n){
+    Z[i,j] <-  abs(theta[i,j]/sqrt(abs(2*(1-theta[i,j]*theta[i,j])/me)))
+  }
+}
+P1 <- 2*pnorm(Z,lower.tail=F)
+
+log_P <- -log10(P1)
+log_P[is.infinite(log_P)] <- 1e-302
+plot(theta,log_P,abline(h = -log10(0.05/(n*(n-1)/2)), col = "red", lty = 2))
+points_above <- which(log_P > -log10(0.05/(n*(n-1)/2)), arr.ind = TRUE)
+points_above2 <- which(theta > 0.4, arr.ind = TRUE)
+hist(P1)
+fdr <- p.adjust(P1, method= "BH")
+hist(fdr)
+#####################
+
 
 ###################################################################################################################
 #卡方检验
